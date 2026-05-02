5b9f476863fe35aebb04653e9323cf2e83e8994a
Simone <smdagondon@up.edu.ph>
2026-05-02 11:29:34 +0800

    feat: change some stuff

--- backend/features/vehicles/vehicle.route.ts
+++ backend/features/vehicles/vehicle.route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import { addVehicle, updateVehicle, getVehicleByOwnerDriverLicense, deleteVehicle, getExpiredVehicles } from './vehicle.controller';
+import { addVehicle, updateVehicle, deleteVehicle, getVehicles, getVehicleByPlate, getExpiredVehicles } from './vehicle.controller';
 
 const vehicleRouter = Router();
 
-vehicleRouter.post('/add-vehicle', addVehicle);
-vehicleRouter.put('/update-vehicle/:plate_number', updateVehicle);
-vehicleRouter.get('/get-vehicle-by-owner/:license_number', getVehicleByOwnerDriverLicense);
-vehicleRouter.get('/get-expired-vehicles', getExpiredVehicles);
-vehicleRouter.delete('/delete-vehicle/:plate_number', deleteVehicle);
+vehicleRouter.get('/', getVehicles);
+vehicleRouter.get('/expired', getExpiredVehicles);
+vehicleRouter.get('/:plate_number', getVehicleByPlate);
+vehicleRouter.post('/', addVehicle);
+vehicleRouter.put('/:plate_number', updateVehicle);
+vehicleRouter.delete('/:plate_number', deleteVehicle);
 
 export default vehicleRouter;
