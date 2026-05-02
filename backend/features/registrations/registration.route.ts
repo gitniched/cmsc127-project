74434e351d99e0254783c7ce2f85d8dd52ec2fc1
Simone <smdagondon@up.edu.ph>
2026-05-02 11:22:19 +0800

    chore: change some stuff

--- backend/features/registrations/registration.route.ts
+++ backend/features/registrations/registration.route.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { getRegistration, renewRegistration, addRegistration } from './registration.controller';
+import { addRegistration, getRegistrations, getRegistrationByNumber } from './registration.controller';
 
 const registrationRouter = Router();
 
-registrationRouter.get('/get-registration', getRegistration);
-registrationRouter.put('/renew-registration', renewRegistration);
-registrationRouter.post('/add-registration', addRegistration);
+registrationRouter.get('/', getRegistrations);
+registrationRouter.get('/:registration_number', getRegistrationByNumber);
+registrationRouter.post('/', addRegistration);
 
 export default registrationRouter;
