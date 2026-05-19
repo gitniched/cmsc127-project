5ec16c2b93b5f63c1166fa1f38d322055c162729
Nicholas <nopacoma@up.edu.ph>
2026-05-19 21:29:54 +0800

    feat: added simple animations when navigating

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { ROUTES } from './constants/routes';
 export { buildRoute } from './constants/routes';
 import NavBar from './components/ui/NavBar';
@@ -12,34 +12,45 @@ import ViolationList  from './pages/ViolationList';
 import ViolationDetail from './pages/ViolationDetail';
 import Reports        from './pages/Reports';
 
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <div key={location.pathname} className="animate-page-enter flex-1 flex flex-col">
+      <Routes location={location}>
+        {/* Dashboard */}
+        <Route path={ROUTES.dashboard}       element={<Dashboard />} />
+
+        {/* Drivers */}
+        <Route path={ROUTES.drivers}         element={<DriverList />} />
+        <Route path={ROUTES.driverProfile}   element={<DriverProfile />} />
+
+        {/* Vehicles */}
+        <Route path={ROUTES.vehicles}        element={<VehicleList />} />
+        <Route path={ROUTES.vehicleDetail}   element={<VehicleDetail />} />
+
+        {/* Violations */}
+        <Route path={ROUTES.violations}      element={<ViolationList />} />
+        <Route path={ROUTES.violationDetail} element={<ViolationDetail />} />
+
+        {/* Reports */}
+        <Route path={ROUTES.reports}         element={<Reports />} />
+
+        {/* redirect unknown paths to dashboard */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </div>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen flex flex-col">
         <NavBar />
-        <main className="flex-1"></main>
-          <Routes>
-            {/* Dashboard */}
-            <Route path={ROUTES.dashboard}       element={<Dashboard />} />
-
-            {/* Drivers */}
-            <Route path={ROUTES.drivers}         element={<DriverList />} />
-            <Route path={ROUTES.driverProfile}   element={<DriverProfile />} />
-
-            {/* Vehicles */}
-            <Route path={ROUTES.vehicles}        element={<VehicleList />} />
-            <Route path={ROUTES.vehicleDetail}   element={<VehicleDetail />} />
-
-            {/* Violations */}
-            <Route path={ROUTES.violations}      element={<ViolationList />} />
-            <Route path={ROUTES.violationDetail} element={<ViolationDetail />} />
-
-            {/* Reports */}
-            <Route path={ROUTES.reports}         element={<Reports />} />
-
-            {/* redirect unknown paths to dashboard */}
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
+        <main className="flex-1 flex flex-col">
+          <AnimatedRoutes />
+        </main>
       </div>
     </BrowserRouter>
   );
